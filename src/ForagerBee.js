28f6d3f0423f61fc7f370c671ed7428d8b5f6e9c
siwarhkimi <cywarhkimi@gmail.com>
2020-03-20 16:25:50 +0100

    yummy

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,23 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = "find pollen";
+    this.color = this.color;
+    this.food = this.food;
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
+/*the Bee superclass
+ an age property that is set to 10
+ a job property that is set to find pollen
+ a color property inherited from bee that is set to yellow
+ a food property that is inherited from grub
+ an eat method that is inherited from grub
+ a canFly property that is set true
+ a treasureChest property that is set to an empty array []
+ a forage method that allows the bee to add a treasure to the treasureChest*/
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = "make honey";
+    this.color = this.color;
+    this.food = this.food;
+    this.honeyPot = 0;
+  }
+  makeHoney () {
+    this.honeyPot++;
+  }
+  giveHoney () {
+    this.honeyPot--;
+  }
 };
+/*
+ a makeHoney method that adds 1 to that honeyBee's honeyPot
+ a giveHoney method that subtracts 1 from that honeyBee's honeyPot*/
